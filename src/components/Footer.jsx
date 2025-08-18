3862351ed4983aa2a69d7fd9b8e127ed1f5aa637
Ezequiel Coeli <ezecoeli@Mac-mini-de-Ezequiel.local>
2025-08-18 19:26:07 +0200

    Preparando para GitHub Pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer className="bg-white dark:bg-black text-black dark:text-white p-4 text-center mt-auto">
-      © 2025 Todos los derechos reservados.
+      {t('allRightsReserved')}
     </footer>
   );
 }
