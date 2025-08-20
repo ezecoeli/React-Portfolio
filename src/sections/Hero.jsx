18f1373108067086626a0e5743bc1baea29901a2
Ezequiel Coeli <ezecoeli@Mac-mini-de-Ezequiel.local>
2025-08-20 19:25:48 +0200

    update hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import heroPhoto from '../assets/heroPhoto.png';
 import { useTranslation } from 'react-i18next';
+import ReactMarkdown from 'react-markdown';
 
 function Hero() {
   const { t } = useTranslation();
@@ -19,12 +20,12 @@ function Hero() {
       <div className="absolute inset-0 bg-black/40 "></div> 
 
       {/* Texto */}
-      <div className="relative z-10 px-6 banner-text">
+      <div className="banner-text relative z-20">
         <motion.h1
           className="text-[clamp(1.5rem,4vw,4rem)] text-xl sm:text-3xl md:text-5xl font-bold text-white text-center"
           initial={{ width: 0, borderRightWidth: 4 }}
           animate={{ width: "100%", borderRightWidth: 0 }}
-          transition={{ duration: 2.5, ease: "linear" }}
+          transition={{ duration: 2.0, ease: "linear" }}
           style={{
             overflow: "hidden",
             borderRight: "4px solid white",
@@ -39,12 +40,15 @@ function Hero() {
       <div className="relative z-10 px-4 banner-text">
         {/* Nombre */}
         <motion.h2
-          className="mt-4 text-lg sm:text-xl md:text-2xl font-mono text-green-400 relative inline-block"
+          className="glitch-h2 mt-4 text-lg sm:text-xl md:text-2xl font-mono text-green-400 relative inline-block"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1.5, duration: 2 }}
         >
-          {t('name')}
+          <ReactMarkdown components={{ p: 'span' }}>
+            {t('name')}
+          </ReactMarkdown>
+
           <motion.span
             className="absolute ml-1"
             animate={{
