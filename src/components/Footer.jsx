dba7ef27a1bbd5f40095696b574cf36db5df8ef0
Ezequiel <ezecoeli@gmail.com>
2026-01-01 20:38:10 +0100

    update copyright-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
   const { t } = useTranslation();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-white dark:bg-black text-black dark:text-white p-4 text-center mt-auto">
-      {t('allRightsReserved')}
+      © {currentYear} · {t('allRightsReserved')}
     </footer>
   );
 }
